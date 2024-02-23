016b9c04d31e20e9f13ef4a179d274bb3eee8a76
Chipihin Mykhaylo <global256local@gmail.com>
2024-02-24 01:23:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 10;
+let decr = 10;
+
+++incr;
+--decr;
+
+console.log(incr);
+console.log(decr);
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+
+console.log(2 * 4 === 8);
+
+const isChecked = true;
+const isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+console.log(!isChecked || isClose);
